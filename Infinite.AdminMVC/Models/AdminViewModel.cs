2c2392ebc62a250c4895ddc9d45288dd279e27e2
pavankumarch <pavankumarch@ICS-LAP-2324>
2023-01-30 23:54:36 +0530

    Add project files.

---
+++ Infinite.AdminMVC/Models/AdminViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infinite.AdminMVC.Models
+{
+    public class AdminViewModel:Login
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //[Required]
+        //public string Email { get; set; }
+        //[Required]
+        //public string Password { get; set; }
+    }
+    public class Login
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
